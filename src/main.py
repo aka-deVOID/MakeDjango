9643a6ba6794ef7115ba8da81d27109c05e572ee
MahanBi <DirtyMBGamer@gmail.com>
2021-11-24 15:12:14 +0330

    main arg parser

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python
+import argparse
+import os
+import sys
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="Django smithing tools",
+        description="Quick creation of Django project template",
+        epilog="Good Luck...",
+    )
+    parser.add_argument("-a", "--auto", metavar=None)
+    parser.add_argument("-sp", "--startproject", metavar="sp", type=str, help="Custom Template", choices=("rest", "jinja", "graphql"))
+
+    args = parser.parse_args()
+    print(args.sp)
+
+if __name__ == "__main__":
+    main()

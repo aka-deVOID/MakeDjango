e646137ad4c1e3923ef8eb622a98be397590e5f9
MahanBi <DirtyMBGamer@gmail.com>
2021-11-27 12:41:25 +0330

    1.0.1

--- MakeDjango/__init__.py
+++
@@ -1,1 +0,0 @@
-from MakeDjango import main
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="MakeDjango",
-    version="1.0.0",
+    version="1.0.1",
     install_requires=[
         "setuptools",
         "wheel",

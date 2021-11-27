2fc907caf25df05e5551a44fdcd6efb8df3af1d8
MahanBi <DirtyMBGamer@gmail.com>
2021-11-27 07:28:56 +0330

    preparing to package

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
+from src.core.processor import __version__
 
 setup(
     name="MakeDjango",
-    version="0.0.1",
+    version=__version__,
     install_requires=[
         "asgiref==3.4.1",
         "pytz==2021.3",
     ],
-    packages=find_packages()
+    packages=find_packages(),
+    zip_safe=False
 
 )
